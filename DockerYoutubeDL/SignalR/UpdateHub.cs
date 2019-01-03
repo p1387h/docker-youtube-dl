72f9c7289b4f0163f31809817c6b9ed96b5d1a5e
P H <ph1387@t-online.de>
2019-01-03 23:36:19 +0100

    Added removal of db entries for disconnecting users

--- DockerYoutubeDL/SignalR/UpdateHub.cs
+++ DockerYoutubeDL/SignalR/UpdateHub.cs
@@ -1,4 +1,5 @@
-﻿using DockerYoutubeDL.Services;
+﻿using DockerYoutubeDL.DAL;
+using DockerYoutubeDL.Services;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -16,14 +17,17 @@ namespace DockerYoutubeDL.SignalR
         private UpdateClientContainer _container;
         private DownloadPathGenerator _pathGenerator;
         private IConfiguration _config;
+        private DownloadContext _context;
 
         public UpdateHub(
             ILogger<UpdateHub> logger, 
             UpdateClientContainer container, 
             DownloadPathGenerator pathGenerator, 
-            IConfiguration config)
+            IConfiguration config,
+            DownloadContext context)
         {
-            if (logger == null || container  == null || pathGenerator == null || config == null)
+            if (logger == null || container  == null || pathGenerator == null || 
+                config == null || context == null)
             {
                 throw new ArgumentException();
             }
@@ -32,6 +36,7 @@ namespace DockerYoutubeDL.SignalR
             _container = container;
             _pathGenerator = pathGenerator;
             _config = config;
+            _context = context;
         }
 
         public override Task OnConnectedAsync()
@@ -45,7 +50,7 @@ namespace DockerYoutubeDL.SignalR
             return base.OnConnectedAsync();
         }
 
-        public override Task OnDisconnectedAsync(Exception exception)
+        public override async Task OnDisconnectedAsync(Exception exception)
         {
             var name = Context.User.Identity.Name;
             var identifier = new Guid(name);
@@ -57,13 +62,29 @@ namespace DockerYoutubeDL.SignalR
             // Ensures that the server frees memory and does not keep allocating space.
             if(_config.GetValue<bool>("DeleteDownloadsOnDisconnect"))
             {
-                var folderPath = _pathGenerator.GenerateDownloadFolderPath(identifier);
-                Directory.Delete(folderPath, true);
+                _logger.LogDebug($"Deleting entries of user {name}");
 
-                _logger.LogDebug($"Deleted downloads of use {name}");
+                await this.DeleteDownloadEntriesAsync(identifier);
             }
 
-            return base.OnDisconnectedAsync(exception);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private async Task DeleteDownloadEntriesAsync(Guid identifier)
+        {
+            // Remove any folders / downloads of the user.
+            var folderPath = _pathGenerator.GenerateDownloadFolderPath(identifier);
+            Directory.Delete(folderPath, true);
+
+            // Entries of the user must be removed from the db.
+            var toDeleteTasks = _context.DownloadTask
+                .Where(x => x.Downloader == identifier);
+            var toDeleteResults = _context.DownloadResult
+                .Where(x => x.IdentifierDownloader == identifier);
+            _context.DownloadTask.RemoveRange(toDeleteTasks);
+            _context.DownloadResult.RemoveRange(toDeleteResults);
+
+            await _context.SaveChangesAsync();
         }
     }
 }
