018b4e1ea2d430f09869833e7798e04f2924450d
P H <ph1387@t-online.de>
2019-01-06 00:52:24 +0100

    Added basic visual representation of downloads

--- DockerYoutubeDL/wwwroot/js/site.js
+++ DockerYoutubeDL/wwwroot/js/site.js
@@ -3,6 +3,33 @@
 
 // Write your Javascript code.
 $(document).ready(function () {
+    let chevronDown = function () {
+        return $.parseHTML("<span class=\"glyphicon glyphicon-chevron-down\"></span>");
+    }
+
+    let glyphiconMinus = function () {
+        return $.parseHTML("<span class=\"glyphicon glyphicon-minus\"></span>");
+    }
+
+    let stateMachine = function () {
+        this.states = ["Waiting", "Downloading", "Converting", "Finished"];
+        let state = 0;
+
+        this.nextState = function () {
+            if (state < states.length() - 1) {
+                state++;
+            }
+        }
+
+        this.getState = function () {
+            return state;
+        }
+
+        this.getNamedState = function () {
+            return this.states[state];
+        }
+    }
+
     $("#buttonDownload")[0].addEventListener("click", function () {
         let input = $("#inputDownload")[0];
         let url = input.value;
@@ -48,29 +75,28 @@ $(document).ready(function () {
                 console.log(error);
             });
 
-        let chevronDown = function () {
-            return $.parseHTML("<span class=\"glyphicon glyphicon-chevron-down\"></span>");
-        }
-
-        let glyphiconMinus = function () {
-            return $.parseHTML("<span class=\"glyphicon glyphicon-minus\"></span>");
-        }
-
         let addListHeader = function (guid, url) {
             let fileEntry = $("#templateFileEntry").clone();
 
             // Head:
-            fileEntry.attr("id", "fileEntry_" + guid).attr("hidden", false);
+            fileEntry.attr("id", "fileEntry_" + guid);
             fileEntry.find("#templateHeading").attr("id", "heading_" + guid);
-            fileEntry.find("a").first().text(" " + url).attr({ "data-toggle": "", href: "#body_" + guid }).prepend(glyphiconMinus());
-            fileEntry.find("#templateContainerButtonDownload").attr("id", "containerButtonDownload_" + guid);
-            fileEntry.find("#templateMessageToUser").attr("id", "messageToUser_" + guid);
-            fileEntry.find("#templateLoading").attr("id", "loading_" + guid);
-
+            fileEntry.find("#templateContainerDownloadInfo").attr("id", "containerDownloadInfo_" + guid);
+            fileEntry.find("#templateContainerDownloadInfoText").attr("id", "containerDownloadInfoText_" + guid);
+            fileEntry.find("#templateContainerDownloadInfoConversion").attr("id", "containerDownloadInfoConversion_" + guid);
+            fileEntry.find("#templateContainerDownloadInfoProgress").attr("id", "containerDownloadInfoProgress_" + guid);
+            fileEntry.find("#templateContainerDownloadInfoButtonDownload").attr("id", "containerDownloadInfoButtonDownload_" + guid);
+            // Change the download link to no toggle the body.
+            fileEntry.find("a[href='#templateBody']").first().text(" " + url).attr({ "data-toggle": "", href: "#body_" + guid }).prepend(glyphiconMinus());
             // Body:
             fileEntry.find("#templateBody").attr("id", "body_" + guid);
 
             fileEntry.prependTo("#fileEntries");
+
+            // Show basic text in order to give users visual feedback.
+            let container = fileEntry.find("#containerDownloadInfo_" + guid);
+            container.children("div").hide();
+            container.find("#containerDownloadInfoText_" + guid).show().find("div[class='infoTextContainer']").text("Link queued...");
         }
     });
 
@@ -90,17 +116,68 @@ $(document).ready(function () {
         }
     }
 
+    start();
+
     connection.onclose(async () => {
         await start();
-    })
+    });
+
+    connection.on("Ping", () => {
+        console.log("Replying to ping");
+        connection.invoke("Pong");
+    });
+
+    connection.on("ReceivedDownloadInfo", (downloadResult) => {
+        console.log({ state: "Info received", result: downloadResult });
+
+        let guid = downloadResult.identifierDownloadTask;
+        let container = $("#containerDownloadInfo_" + guid);
+        container.children("div").hide();
+        container.find("#containerDownloadInfoText_" + guid).show().find("div[class='infoTextContainer']").text("Gathering information...");
+    });
+
+    connection.on("DownloadStarted", (taskIdentifier, taskResultIdentifier) => {
+        console.log({ state: "Started", task: taskIdentifier, result: taskResultIdentifier });
+
+        let guid = taskIdentifier;
+        let container = $("#containerDownloadInfo_" + guid);
+        container.children("div").hide();
+        container.find("#containerDownloadInfoText_" + guid).show().find("div[class='infoTextContainer']").text("Starting download...");
+    });
+
+    connection.on("DownloadProgress", (taskIdentifier, resultIdentifier, percentage) => {
+        console.log({ state: "Progress", task: taskIdentifier, result: resultIdentifier, percentage: percentage });
+
+        let guid = taskIdentifier;
+        let container = $("#containerDownloadInfo_" + guid);
+        container.children("div").hide();
+        container.find("#containerDownloadInfoProgress_" + guid).show();
+
+        // Order of percentages might not be correct.
+        let progressBar = container.find(".progress-bar");
+        let currentPercentage = progressBar.text().replace("%","");
+        if (percentage > currentPercentage) {
+            progressBar.attr("style", "width:" + percentage + "%").text(percentage + "%");
+        }
+    });
+
+    connection.on("DownloadConversion", (taskIdentifier, resultIdentifier) => {
+        console.log({ state: "Conversion", task: taskIdentifier, result: resultIdentifier });
+
+        let guid = taskIdentifier;
+        let container = $("#containerDownloadInfo_" + guid);
+        container.children("div").hide();
+        container.find("#containerDownloadInfoProgress_" + guid).show();
+        container.find(".progress-bar").text("Converting...");
+    });
 
     connection.on("DownloadFinished", (taskIdentifier, taskResultIdentifier) => {
         console.log({ state: "Finished", task: taskIdentifier, result: taskResultIdentifier });
 
         let guid = taskIdentifier;
-        let fileEntry = $("#fileEntry_" + guid);
-        fileEntry.find("#loading_" + guid).hide();
-        fileEntry.find("#containerButtonDownload_" + guid).show()
+        let container = $("#containerDownloadInfo_" + guid);
+        container.children("div").hide();
+        container.find("#containerDownloadInfoButtonDownload_" + guid).show()
             .find("a").attr("href", "./download?taskIdentifier=" + guid + "&taskResultIdentifier=" + taskResultIdentifier);
     });
 
@@ -110,35 +187,7 @@ $(document).ready(function () {
         let guid = taskIdentifier;
         let fileEntry = $("#fileEntry_" + guid);
         fileEntry.addClass("panel-danger");
-        fileEntry.find("#loading_" + guid).hide();
-        fileEntry.find("#messageToUser_" + guid).text("Download failed. See the logs for details.").show();
+        fileEntry.find("#containerDownloadInfoText_" + guid).show().find("div[class='infoTextContainer']").text("Download failed. See the logs for further information.");
+        fileEntry.find("#containerDownloadInfoText_" + guid).find("img").hide();
     });
-
-    connection.on("DownloadStarted", (taskIdentifier, taskResultIdentifier) => {
-        console.log({ state: "Started", task: taskIdentifier, result: taskResultIdentifier });
-
-        let guid = taskIdentifier;
-        let fileEntry = $("#fileEntry_" + guid);
-        fileEntry.find("#messageToUser_" + guid).hide();
-        fileEntry.find("#loading_" + guid).show();
-    });
-
-    connection.on("ReceivedDownloadInfo", (downloadResult) => {
-        console.log({ state: "Info received", result: downloadResult });
-    });
-
-    connection.on("DownloadProgress", (taskIdentifier, resultIdentifier, percentage) => {
-        console.log({ state: "Progress", task: taskIdentifier, result: resultIdentifier, percentage: percentage });
-    });
-
-    connection.on("DownloadConversion", (taskIdentifier, resultIdentifier) => {
-        console.log({ state: "Conversion", task: taskIdentifier, result: resultIdentifier });
-    });
-
-    connection.on("Ping", () => {
-        console.log("Replying to ping");
-        connection.invoke("Pong");
-    });
-
-    start();
 });
